5fff62fe24e1cd259548d6a107f49d10924b31cf
Ali <104919540+deliyim3434@users.noreply.github.com>
2025-08-11 21:48:30 +0300

    Update help.py

--- ArchMusic/utils/inline/help.py
+++ ArchMusic/utils/inline/help.py
@@ -1,89 +1,114 @@
+#
+# Copyright (C) 2021-2023 by ArchBots@Github, < https://github.com/ArchBots >.
+#
+# This file is part of < https://github.com/ArchBots/ArchMusic > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/ArchBots/ArchMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
 from typing import Union
+
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from ArchMusic import app
 
 
 def help_pannel(_, START: Union[bool, int] = None):
-    """
-    Yardım paneli butonlarını döndürür.
-    START parametresi True ise geri butonu da eklenir.
-    """
-
-    # Son satırdaki kontrol butonları (geri/kapat)
-    control_buttons = [
-        InlineKeyboardButton(text=_["BACK_BUTTON"], callback_data="settingsback_helper"),
-        InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data="close"),
-    ] if START else [
-        InlineKeyboardButton(text=_["CLOSEMENU_BUTTON"], callback_data="close")
+    first = [
+        InlineKeyboardButton(
+            text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
+        )
     ]
-
-    # Yardım butonları verisi
-    button_data = [
-        ("H_B_1", "hb1"), ("H_B_2", "hb2"),
-        ("H_B_3", "hb3"), ("H_B_4", "hb4"),
-        ("H_B_5", "hb5"), ("H_B_6", "hb6"),
-        ("H_B_7", "hb7"), ("H_B_8", "hb8"),
-        ("H_B_9", "hb9"),
+    second = [
+        InlineKeyboardButton(
+            text=_["BACK_BUTTON"],
+            callback_data=f"settingsback_helper",
+        ),
+        InlineKeyboardButton(
+            text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
+        ),
     ]
-
-    # Butonları 3'lü gruplar halinde satırlara yerleştir
-    keyboard = []
-    for i in range(0, len(button_data), 3):
-        row = []
-        for j in range(3):
-            if i + j < len(button_data):
-                text_key, cb_key = button_data[i + j]
-                row.append(
-                    InlineKeyboardButton(text=_[text_key], callback_data=f"help_callback {cb_key}")
-                )
-        keyboard.append(row)
-
-    # Kontrol butonunu ekle
-    keyboard.append(control_buttons)
-
-    return InlineKeyboardMarkup(keyboard)
-
-
-def help_back_markup(_):
-    """
-    Yardım alt menüsünde geri ve kapat butonlarını döndürür.
-    """
-    return InlineKeyboardMarkup(
+    mark = second if START else first
+    upl = InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton(
-                    text=_["BACK_BUTTON"], callback_data="settingsback_helper"
-                ),
-                InlineKeyboardButton(
-                    text=_["CLOSEMENU_BUTTON"], callback_data="close"
-                ),
-            ]
-        ]
-    )
-
-
-def private_help_panel(_):
-    """
-    Özel mesajlarda gösterilecek yardım ve geri butonlarını döndürür.
-    Kullanıcıyı botla özelde yardıma yönlendirir.
-    """
-    return InlineKeyboardMarkup(
-        [
+                InlineKeyboardButton(text=_["H_B_1"],callback_data="help_callback hb1",),
+                 InlineKeyboardButton( text=_["H_B_2"],callback_data="help_callback hb2",), 
+            ],
+            [
+            ],
             [
                 InlineKeyboardButton(
-                    text=_["S_B_1"],
-                    url=f"https://t.me/{app.username}?start=help",
+                    text=_["H_B_3"],
+                    callback_data="help_callback hb3",
+                ),
+                InlineKeyboardButton(
+                    text=_["H_B_4"],
+                    callback_data="help_callback hb4",
+                    
                 ),
             ],
             [
                 InlineKeyboardButton(
-                    text=_["BACK_BUTTON"], callback_data="settingsback_helper"
+                    text=_["H_B_5"],
+                    callback_data="help_callback hb5",
+                ),
+            
+                InlineKeyboardButton(
+                    text=_["H_B_6"],
+                    callback_data="help_callback hb6",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["H_B_7"],
+                    callback_data="help_callback hb7",
                 ),
                 InlineKeyboardButton(
-                    text=_["CLOSEMENU_BUTTON"], callback_data="close"
+                    text=_["H_B_8"],
+                    callback_data="help_callback hb8",
+                ),
+            ],
+            [
+                InlineKeyboardButton(
+                    text=_["H_B_9"],
+                    callback_data="help_callback hb9",
+                ),
+                
+            ],
+            
+            mark,
+        ]
+    )
+    return upl
+
+
+def help_back_markup(_):
+    upl = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    text=_["BACK_BUTTON"],
+                    callback_data=f"settings_back_helper",
+                ),
+                InlineKeyboardButton(
+                    text=_["CLOSE_BUTTON"], callback_data=f"close"
                 ),
             ]
         ]
     )
+    return upl
 
+
+def private_help_panel(_):
+    buttons = [
+        [
+            InlineKeyboardButton(
+                text=_["S_B_1"],
+                url=f"https://t.me/{app.username}?start=help",
+            ),
+        ],
+    ]
+    return buttons
