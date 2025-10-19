a3f8bb04ed86dbdcedca04c6d8ad4f3e7fe4c4aa
Ali <104919540+deliyim3434@users.noreply.github.com>
2025-10-19 21:19:19 +0300

    Update help.py

--- ArchMusic/utils/inline/help.py
+++ ArchMusic/utils/inline/help.py
@@ -37,29 +37,17 @@ def help_pannel(_, START: Union[bool, int] = None):
                 InlineKeyboardButton(text=_["H_B_1"],callback_data="help_callback hb1",),
                  InlineKeyboardButton( text=_["H_B_2"],callback_data="help_callback hb2",), 
             ],
-            [
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_3"],
-                    callback_data="help_callback hb3",
-                ),
-                InlineKeyboardButton(
-                    text=_["H_B_4"],
-                    callback_data="help_callback hb4",
-                    
-                ),
-            ],
-            [
-                InlineKeyboardButton(
-                    text=_["H_B_5"],
-                    callback_data="help_callback hb5",
-                ),
             
-                InlineKeyboardButton(
-                    text=_["H_B_6"],
-                    callback_data="help_callback hb6",
-                ),
+            [
+                InlineKeyboardButton(text=_["H_B_3"],callback_data="help_callback hb3",),
+                 InlineKeyboardButton(  text=_["H_B_4"], callback_data="help_callback hb4",),
+                    
+                
+                
+                    
+                    
+                    
+                
             ],
             
             
